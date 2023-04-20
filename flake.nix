263e8728a3e7a52101ac5471b821235ea98fc255
Ilan Joselevich <personal@ilanjoselevich.com>
2023-04-20 17:10:40 +0300

    test

--- flake.nix
+++ flake.nix
@@ -1,41 +1,88 @@
 {
   description = "NixOS images";
 
-  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-  inputs.nixos-2211.url = "github:NixOS/nixpkgs/release-22.11";
-
-  nixConfig.extra-substituters = [
-    "https://cache.garnix.io"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-  ];
-
-  outputs = { self, nixos-unstable, nixos-2211 }: let
-    supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
-    forAllSystems = nixos-unstable.lib.genAttrs supportedSystems;
-  in {
-    packages = forAllSystems (system: let
-      netboot = nixpkgs: (import (nixpkgs + "/nixos/release.nix") {}).netboot.${system};
-      kexec-installer = nixpkgs: (nixpkgs.legacyPackages.${system}.nixos [self.nixosModules.kexec-installer]).config.system.build.kexecTarball;
-    in {
-      netboot-nixos-unstable = netboot nixos-unstable;
-      netboot-nixos-2211 = netboot nixos-2211;
-      kexec-installer-nixos-unstable = kexec-installer nixos-unstable;
-      kexec-installer-nixos-2211 = kexec-installer nixos-2211;
-    });
-    nixosModules.kexec-installer = import ./nix/kexec-installer/module.nix;
-    checks.x86_64-linux = let
-      pkgs = nixos-unstable.legacyPackages.x86_64-linux;
-    in {
-      kexec-installer-unstable = pkgs.callPackage ./nix/kexec-installer/test.nix {};
-      shellcheck = pkgs.runCommand "shellcheck" {
-        nativeBuildInputs = [ pkgs.shellcheck ];
-      } ''
-        shellcheck ${(pkgs.nixos [self.nixosModules.kexec-installer]).config.system.build.kexecRun}
-        touch $out
-      '';
-      kexec-installer-2211 = nixos-2211.legacyPackages.x86_64-linux.callPackage ./nix/kexec-installer/test.nix {};
-    };
+  inputs = {
+    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixos-2211.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixos-unstable"; };
+    hercules-ci-effects = { url = "github:Kranzes/hercules-ci-effects"; inputs.nixpkgs.follows = "nixos-unstable"; };
   };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ config, ... }: {
+      systems = [ "x86_64-linux" ]; # "aarch64-linux" ];
+      imports = [ inputs.hercules-ci-effects.flakeModule ];
+
+      flake.nixosModules.kexec-installer = import ./nix/kexec-installer/module.nix;
+
+      perSystem = { inputs', system, pkgs, pkgs2211, ... }: {
+        _module.args = {
+          pkgs = inputs'.nixos-unstable.legacyPackages;
+          pkgs2211 = inputs'.nixos-2211.legacyPackages;
+        };
+
+        packages =
+          let
+            netboot = nixpkgs: (import (nixpkgs + "/nixos/release.nix") { }).netboot.${system};
+            kexec-installer = pkgs: (pkgs.nixos [ inputs.self.nixosModules.kexec-installer ]).config.system.build.kexecTarball;
+          in
+          {
+            netboot-nixos-unstable = netboot inputs.nixos-unstable;
+            #  netboot-nixos-2211 = netboot inputs.nixos-2211;
+            kexec-installer-nixos-unstable = kexec-installer pkgs;
+            #  kexec-installer-nixos-2211 = kexec-installer pkgs2211;
+          };
+
+        #checks = {
+        #  kexec-installer-unstable = pkgs.callPackage ./nix/kexec-installer/test.nix { };
+        #  shellcheck = pkgs.runCommand "shellcheck" { } "${pkgs.lib.getExe pkgs.shellcheck} ${(pkgs.nixos [inputs.self.nixosModules.kexec-installer]).config.system.build.kexecRun} && touch $out";
+        #};
+        devShells.default = pkgs.mkShell { packages = [ pkgs.hci ]; };
+      };
+
+      hercules-ci = {
+        flake-update = {
+          enable = true;
+          updateBranch = "main";
+          createPullRequest = true;
+          autoMergeMethod = "merge";
+          # Update  everynight at midnight
+          when = {
+            hour = [ 0 ];
+            minute = 0;
+          };
+        };
+
+        github-releases = {
+          systems = config.systems;
+          releaseTag = _: "test123";
+          condition = { branch, ... }: (branch == "main");
+          filesPerSystem = { pkgs, self', system, ... }: [
+            {
+              label = "nixos-kexec-installer-${system}.tar.gz";
+              path = "${self'.packages.kexec-installer-nixos-unstable}/nixos-kexec-installer-${system}.tar.gz";
+            }
+            {
+              label = "bzImage-${system}";
+              path = "${self'.packages.netboot-nixos-unstable}/bzImage";
+            }
+            {
+              label = "initrd-${system}";
+              path = "${self'.packages.netboot-nixos-unstable}/initrd";
+            }
+            {
+              label = "netboot-${system}.ipxe";
+              path = "${self'.packages.netboot-nixos-unstable}/netboot.ipxe";
+            }
+            {
+              label = "sha256sums-${system}";
+              path = pkgs.runCommand "sha256sums-${system}" { } ''
+                ${pkgs.lib.concatMapStringsSep "\n"({ label, path }: if label == "sha256sums-${system}" then "true" else "ln -s ${path} ${label}") config.hercules-ci.github-releases.finalFiles}
+                sha256sum * > "$out"
+              '';
+            }
+          ];
+        };
+      };
+    });
 }
